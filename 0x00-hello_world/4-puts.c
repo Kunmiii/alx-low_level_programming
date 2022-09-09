489ae8dba39e31624382c7e8d28d2a5ade4c8105
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-08 19:36:48 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - this is the entry point for the program
- * return: returns 0 if no errors otherwise returns non-zero
+ *Main - this is the entry point for the program
+ *Return: By default returns zero if no errors otherwise returns non-zero
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
