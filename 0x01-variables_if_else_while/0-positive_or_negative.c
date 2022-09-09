2c69e2053695f50d7df79d2653c95c9f7c5c2914
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-09 08:20:05 -0700

    Readme updated, generate +ve, or -ve number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point for the program 
+ * Description: program generates and prints random numbers
+ * Return: zero is returned for successful compilation, otherwise non-zero
+ */
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* check if generated number is +ve, zero or -ve */
+		if ( n > 0)
+		{
+			printf("%i, is positive\n", n);
+		}
+	       	else if (n == 0) 
+		{
+			printf("%i, is zero\n", n);
+		}
+		else
+		{
+			printf("%i, is negative\n", n);
+		}
+		return (0);
+
+}
