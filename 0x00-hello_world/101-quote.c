29eab1c40d902637ff523634ff459e287692be48
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-08 20:33:48 -0700

    print exact strings

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
  * main - this is the entry point to the program
  * Description: prints exact string followed by a new line
- * Return: 1
+ * Return: return 1 after success
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
