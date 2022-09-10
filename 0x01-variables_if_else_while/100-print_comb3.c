ce4ddc72e4a819e217610db224536739ab2299dc
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-10 09:24:29 -0700

    combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ **/
+int main(void)
+{
+
+		int i, j, k;
+
+		for (i = 0; i < 100; i++)
+		{
+			j = (i / 10);
+			k = (i % 10);
+			
+			if (k < j)
+			{
+				j = (j + '0');
+				k = (k + '0');
+
+				putchar(j);
+				putchar(k);
+			}
+				
+			if (i = 89)
+			{
+				break;
+			}
+			putchar(44);
+			putchar(32);
+		}
+			putchar('\n');
+
+		return (0);
+
+}
