c7735548565d13fca75a0475c50598d6a49d907e
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-10 07:26:05 -0700

    numbers 0-9 in ASC with space and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ **/
+int main(void)
+{
+
+		int i;
+
+		for (i = 48; i < 58; i++)
+		{
+			putchar(i);
+			putchar(44);
+			putchar(32);
+		}
+			putchar('\n');
+
+		return (0);
+
+}
