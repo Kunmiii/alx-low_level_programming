a1c6cfbbfb70d7ba2abb00ea6bc1f7be32eb17ce
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-10 07:36:53 -0700

    numbers 0-9 in ASC with space and comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,11 @@ int main(void)
 		for (i = 48; i < 58; i++)
 		{
 			putchar(i);
-			putchar(44);
 			if (i == 57)
+			{
 				break;
-
+			}
+			putchar(44);
 			putchar(32);
 		}
 			putchar('\n');
