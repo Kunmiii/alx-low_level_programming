dcabca64301a578b0ced757e4dcb774653a477ad
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-10 04:46:04 -0700

    single digit from 0 to 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program entry
+ * Description: prints all single digit of base 10
+ * starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+}
