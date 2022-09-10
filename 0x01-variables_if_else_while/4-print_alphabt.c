5e10eddf25c5478551a699ab18ed3376232c6e08
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-10 04:20:38 -0700

    omit letter q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ * Description: THis program prints alphabets a-z
+ * excluding leter q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char xter = 'a';
+
+	while (xter <= 'z')
+	{
+		if (xter != 'e' && xter != 'q')
+		putchar(xter);
+		xter++;
+	}
+}
