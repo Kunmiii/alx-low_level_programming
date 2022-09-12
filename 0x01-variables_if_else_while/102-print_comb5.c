502bfe5ae9d75e66eb840ae1c60e3688fe7fdb73
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-12 10:27:28 -0700

    description added

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,22 +26,27 @@ int main(void)
 		{
 			c = (j / 10);
 			d = (j % 10);
+		   
+			if ( a < c || (a == c && b < d))
+			{	
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
 
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
 
-			putchar(c + '0');
-			putchar(d + '0');
+				if (i == 98 && j == 99)
+					break;
 
-			if (i == 98 && j == 99)
-				break;
+				putchar(44);
+				putchar(32);
+			}
 
-			putchar(44);
-			putchar(32);
 		}
 
 	}
+		putchar(10);
 
-	return (0);
+		return(0);
 }
