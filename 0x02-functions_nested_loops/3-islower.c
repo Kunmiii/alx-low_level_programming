6ba92b160120c33e6336686f75c33a3df63f3bdd
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-13 18:54:59 -0700

    checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - character is lowercase returns 1 , otherwise 0.
+ * @c: returns 1 if c  is lowercase
+ * Return: 1 if lowercase,otherwise 0 if lowercase
+ */
+int _islower(int c)
+{
+	c = islower('b');
+
+	if (c == 1)
+		return (1);
+	else
+		return (0);
+}
