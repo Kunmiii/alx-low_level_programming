7d574ca8361e32b6126f6c1fed5154840ad6a137
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-13 19:20:32 -0700

    checks for uppercase charcter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - character is uppercase returns 1 , otherwise 0.
+ * @c: returns 1 if c  is uppercase
+ * Return: 1 if uppercase, otherwise 0 if lowercase
+ */
+int _isalpha(int c)
+{
+
+	if (c > 64 c < 91)
+		return (1);
+	else
+		return (0);
+}
