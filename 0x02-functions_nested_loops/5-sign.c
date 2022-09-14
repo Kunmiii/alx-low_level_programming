052ea42aa46c5a26fc36943e6cb194fb4b1f7e53
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-13 20:12:15 -0700

    prints sign of numner

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number.
+ * @n: returns 1 and prints + if @n: is positive
+ * @n: returns 0 and prints 0 if @n: is neutral
+ * @n: returns -1 and prints - if @n: is negative
+ * Return: 0 always
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48)
+		return (0);
+	}
+}
