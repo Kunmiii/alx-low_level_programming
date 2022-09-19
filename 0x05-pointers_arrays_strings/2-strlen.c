2637320c0123fa143a77afeda757db88b51b6473
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 16:28:30 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function returns the length of a string
+ * @s: char pointer type
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
