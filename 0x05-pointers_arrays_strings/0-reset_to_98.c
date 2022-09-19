69573bbce6623e1da952c7b43d4629018d528908
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 14:22:17 -0700

    description added

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,8 @@
 /**
  * reset_to_98: takes a pointer to an int as
  * a paramter and updates the value it points to 98
+ * @n: int type pointer
+ * Description: pointer varaiable to update a value
  */
 void reset_to_98(int *n)
 {
