53072afc83ab2e2afa2ca2adff7b39b90c690379
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 15:09:41 -0700

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap.c - Don't swap horses in crossing a stream
+ * @a: pointer integer a
+ * @b: pointer integer b
+ * Description: swap values of two integers
+ */
+void swap_int(int *a, int *b)
+{
+	int *temp;
+
+	temp = &b;
+	b = *a;
+	a = *temp;
+}
