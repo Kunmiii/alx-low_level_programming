4291b5c174d84275eb774a614f1f219068bb07d4
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 15:40:12 -0700

    temp pointer corrected

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * 1-swap.c - Don't swap horses in crossing a stream
+ * swap_int - dont swap horses in crossing a stream
  * @a: pointer integer a
  * @b: pointer integer b
- *
  * Description: swap values of two integers
  */
 void swap_int(int *a, int *b)
