1c37a645db45a492cc59d1cd1352b64d52410d9a
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 15:27:41 -0700

    pointer variable removed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * swap.c - Don't swap horses in crossing a stream
+ * 1-swap.c - Don't swap horses in crossing a stream
  * @a: pointer integer a
  * @b: pointer integer b
+ *
  * Description: swap values of two integers
  */
 void swap_int(int *a, int *b)
 {
-	int *temp;
+	int temp;
 
-	temp = &b;
+	temp = *b;
 	b = *a;
 	a = *temp;
 }
