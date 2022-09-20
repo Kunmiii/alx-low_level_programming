bf539735c728e3f575d732a4aae2260c4ee565b1
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 20:30:35 -0700

    prints every other xter

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		if (str[i] == '\0')
-			break;
-		_putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i += 1)
+	
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 
 	_putchar('\n');
