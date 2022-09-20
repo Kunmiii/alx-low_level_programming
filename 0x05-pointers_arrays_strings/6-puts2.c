440e65c83127cd50ba2ea5a79f93943e36e5d0fa
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 19:23:59 -0700

    prints every other xter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts2 - prints every other xters of string
+ * starting from the first xter
+ * @str: string type char
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i % 2 == 0; i = i + 2)
+	{
+		puts(i);
+	}
+}
