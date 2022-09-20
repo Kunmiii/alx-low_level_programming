487dc0d78ae6dd7b9f81d3ad4502443950078149
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-19 17:40:15 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev - prints a string inreverse, 
+ * followed by a new line
+ * @s: pointer type char
+ */
+void print_rev(char *s)
+{
+	int m = strlen(s);
+
+	for (int i = m; i <= 0; i--)
+		puts(s[i]);
+}
