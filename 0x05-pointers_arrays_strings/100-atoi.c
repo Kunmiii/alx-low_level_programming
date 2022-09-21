9e6691b1d781f6cd6d0c217f42dca92271cbc955
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-20 18:56:43 -0700

    trailing spaces removed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,13 +2,15 @@
 /**
  * _atoi - string conversion to integer
  * @s: array string char
- * Description:  number in the string can be preceded by an infinite number of characters
+ * Description:  number in the string can be preceded
+ * by an infinite number of characters
  * You need to take into account all the - and + signs before the number
  * If there are no numbers in the string, the function must return 0
  * You are not allowed to use long
  * You are not allowed to declare new variables of “type” array
  * You are not allowed to hard-code special values
- * We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.
+ * We will use the -fsanitize=signed-integer-overflow
+ * gcc flag to compile your code.
  * Return: first integer found in string
  */
 int _atoi(char *s)
