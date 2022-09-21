c28d133fdbf79504357ad9f6356250caeda5edbf
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-20 19:19:02 -0700

    generate random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>                                                               #include <stdlib.h>
+
+int main(void)                                                                  {
+
+
+
+	int myrand;
+
+	int count;
+
+	int total;
+
+
+
+	srand(time(NULL));
+
+	for (count = 0, total = 2772; total > 122; count++)
+
+	{
+
+		myrand = (rand() % 125) + 1;
+
+		printf("%c", myrand);
+
+		total -= myrand;
+
+	}
+		                                                                return (0);
+}
