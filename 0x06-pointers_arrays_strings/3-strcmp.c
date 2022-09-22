69582a5ca8639eebada45a62858326a36da36ab7
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-22 11:07:38 -0700

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: char datatype for string 1
+ * @s2: char datatype for string 2
+ *
+ * Return: 0 if s1 and s2 are the same;
+ * less than 0 if s1<s2; greater than 0 if s1>s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return(strcmp(s1, s2));
+}
