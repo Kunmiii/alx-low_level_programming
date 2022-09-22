dd63b91fe60ea667dc825e9a35c963cb1dbc8710
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-22 09:34:08 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest : char data type for destination
+ * @src: char data type for source
+ * Description: Append the src string to the dest string
+ * overwriting the terminating null byte at the end of dest
+ * and then adds a terminating null byte
+ *
+ * Returns: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
