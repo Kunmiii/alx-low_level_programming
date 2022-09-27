119c1038d72195240ba548f564215ed9025c7f1b
Oluwatimileyin <oke_joast@protonmail.com>
2022-09-26 21:58:32 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - copy memory are
+ * @dest: char array to copy inti
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	return (memcpy(dest, src, n));
+
+}
