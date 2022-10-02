58384d5cda181bdaeebf6491cb76750ff0d424ca
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-02 13:34:04 -0700

    copies memory are

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,6 +10,17 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-	return (memcpy(dest, src, n));
+	char *temp;
 
+	temp = dest;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (temp);
 }
