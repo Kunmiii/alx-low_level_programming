cbcba868d797b2c06864395f0788650ff943de1d
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-04 18:08:03 -0700

    adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,23 +23,15 @@ int main(int argc, char *argv[])
 	total = 0;
 
 	if (argc > 1)
-
 	{
-
 		for (i = 1; argv[i]; i++)
-
 		{
-
 			num = strtol(argv[i], &p, 10);
 
 			if (!*p)
-
 				total += num;
-
 			else
-
 			{
-
 				printf("Error\n");
 
 				return (1);
@@ -48,4 +40,7 @@ int main(int argc, char *argv[])
 
 		}
 	}
+	printf("%d\n", total);
+
+	return (0);
 }
