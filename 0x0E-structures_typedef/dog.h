2e0967b31a6ac026d8790a738a678568e6b9c70f
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-10 08:01:03 -0700

    Poppy got a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - Poppy
+ * @name: type char pointer
+ * @age: type float
+ * @owner: type char pointer
+ *
+ * Description: Define a new type struct dog 
+ */
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
