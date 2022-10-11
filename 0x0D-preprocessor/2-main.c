a75fcdd799db34945fb2da9b4e022e7d579b0d28
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-10 21:46:28 -0700

    File Name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file compiled from
+ * Return: 0;
+ */
+
+int main(void)
+{
+	_putchar("%s\n", __FILE__);
+	return (0);
+}
