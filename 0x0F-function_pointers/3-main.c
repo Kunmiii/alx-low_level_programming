b9d5a4a02e5e1cc1679ef0d0cfb81b358ae8cd3a
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-12 16:19:08 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*o)(int, int);
+
+	if (argc != 4)
+	{
+		_putchar("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		_putchar("Error\n");
+		exit(99);
+	}
+
+	o = get_op_func(argv[2]);
+
+	if (o == NULL)
+	{
+		_putchar("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	_putchar("%d\n", o(a, b));
+	return (0);
+}
