69b32e853fda769ce7ebca6a799ebd4df167d690
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-20 06:53:21 -0700

    frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+
+	list_t *tmp = head;
+	list_t *new;
+
+	while (tmp)
+	{
+		new = tmp->next;
+		free(tmp->str);
+		free(tmp);
+		tmp = new;
+	}
+}
