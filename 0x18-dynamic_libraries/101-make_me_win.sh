f223d1107c31655c13587822c376afeeb7bcadae
Oluwatimileyin <oke_joast@protonmail.com>
2022-12-12 19:12:59 -0800

    file updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Adikaibe-Sandra/
+wget -P .. https://raw.githubusercontent.com/Kunmiii/
 alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
